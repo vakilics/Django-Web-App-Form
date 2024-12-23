2e03e09d05cdd149a53bbe3c3733d16b5d74d86f
Vakili, Abdul Rahman <vakili@univention.de>
2024-12-23 16:18:50 +0100

    feat: add initial Django app for job application

---
+++ main.py
@@ -0,0 +1,5 @@
+# django-admin startproject mysite .
+#1. create new app:
+#2. python3 manage.py startapp job_application
+#3. Then, mysite -> settings.py -> change "INSTALLED_APPS" and add  "'job_application',"
+#4. python3 manage.py runserver
